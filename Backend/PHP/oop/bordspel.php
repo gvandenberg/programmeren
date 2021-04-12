e983d5f0418b81988d6c6522ea55ad1a3a2ac96b
Gert van den Berg <78845676+gvandenberg@users.noreply.github.com>
2021-04-12 07:04:53 +0200

    bordspel

---
+++ Backend/PHP/oop/bordspel.php
@@ -0,0 +1,31 @@
+<?php
+
+class Money {
+    public $value;
+    public $color;
+
+
+    function set_value($value) {
+        $this->value = $value;
+    }
+
+    function get_value() {
+        return $this->value;
+    }
+}
+
+class pieces {
+    public $name;
+    public $shape;
+    public $color;
+
+    function set_name($name) {
+        $this->name = $name;
+    }
+
+    function get_name() {
+        return $this->name;
+    }
+}
+
+?>
